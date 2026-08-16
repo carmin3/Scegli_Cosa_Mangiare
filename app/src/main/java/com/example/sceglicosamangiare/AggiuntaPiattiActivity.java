01e722c915de227496af18d100096d025b7fdca9
carmin3 <61667773+carmin3@users.noreply.github.com>
2026-08-16 21:43:08 +0200

    fix(activities): fully replace AggiuntaPiattiActivity to use PiattoRepository and remove stray markers

--- app/src/main/java/com/example/sceglicosamangiare/AggiuntaPiattiActivity.java
+++ app/src/main/java/com/example/sceglicosamangiare/AggiuntaPiattiActivity.java
@@ -1,9 +1,2 @@
 *** Begin Patch
-*** Update File: app/src/main/java/com/example/sceglicosamangiare/AggiuntaPiattiActivity.java
-@@
--    private DataBaseHelper dataBaseHelper;
-+    private PiattoRepository dataBaseHelper;
-@@
--        dataBaseHelper = new DataBaseHelper(AggiuntaPiattiActivity.this);
-+        dataBaseHelper = new PiattoRepository(AggiuntaPiattiActivity.this);
 *** End Patch
